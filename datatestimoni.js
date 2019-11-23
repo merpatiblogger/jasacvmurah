df23bfa3d9489918b226115e0f4a2145e89de86a
merpatiblogger <merpatiblogger@gmail.com>
2019-11-23 13:06:03 +0700

    Update datatestimoni.js

--- datatestimoni.js
+++ datatestimoni.js
@@ -1,19 +1,19 @@
 var dataOrder = {
-    "+62 878-6354-66...": {
+    "+888888": {
         "screenshotURL": "https://lh3.googleusercontent.com/u/0/d/1Ivhr9Wc6ftJzWmewbZZ1uyZXqYhhaHr9=w500",
         "avaURL": "",
         "time": "2019-05-23",
         "template": "LITE Media - Premium Blogger Template",
         "templateURL": "https://www.seotemplate.web.id/2019/06/lite-media-premium-blogger-template.html",
     },
-    "+62 823-5211-47...": {
+    "++888888": {
         "screenshotURL": "https://i.ibb.co/m8XzsL1/image.png",
         "avaURL": "https://i.ibb.co/gD3svLG/image.png",
         "time": "2019-07-21",
         "template": "TOKO WhatsApp - Premium Blogger Template",
         "templateURL": "https://www.seotemplate.web.id/2019/05/toko-whatsapp-premium-blogger-template.html",
     },
-    "+62 822-7137-98...": {
+    "++888888": {
         "screenshotURL": "https://i.ibb.co/QCKXJhL/image.png",
         "avaURL": "https://i.ibb.co/F0sbBxx/image.png",
         "time": "2019-06-15",
